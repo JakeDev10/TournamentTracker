4b597d82668e9e96ea22f750c594352f3e08349b
JakeThompson <JThompson@dev-10.com>
2022-04-07 15:20:10 -0500

    viewParticipants(), saveChanges(), and exit() complete.

--- TournamentTracker.py
+++ TournamentTracker.py
@@ -1,6 +1,8 @@
+import csv
 
 participantList = []
 exitVar = 0
+validInput = 0
 
 def signUp():
     print("Participant Sign Up\n====================")
@@ -9,12 +11,12 @@ def signUp():
     name = input("Participant name: ")
 
     while validInput == 0:
-        slot = input(f"desired starting slot [1-{numParticipants}]")
+        slot = input(f"Starting slot #[1-{numParticipants}]: ")
 
         if not slot.isnumeric():                            #if slot is not all numbers
             print("Invalid input, please enter a number")
-        elif int(slot) > numParticipants:
-            print("There's not that many slots, try again")
+        elif int(slot) > numParticipants or int(slot) < 1:
+            print("Error, that slot does not exist")
         elif participantList[int(slot)-1] != 'None':             #if slot is occupied
             print(f"Slot #{slot} is taken, try another")
         else:
@@ -26,10 +28,12 @@ def cancelSignUp():
     validInput = 0
 
     while validInput == 0:
-        slot = input("Starting slot: ")
+        slot = input(f"Starting slot #[1-{numParticipants}]: ")
         name = input("Participant name: ")
 
-        if (not slot.isnumeric()) or int(slot) > numParticipants:
+        if (not slot.isnumeric()):
+            print("Invalid input, slot must be a number")
+        elif int(slot) > numParticipants or int(slot) < 1:
             print(f"Invalid input, slot must be a number in range [1-{numParticipants}]")
         elif participantList[int(slot)-1] != name:
             print(f"Error, {name} is not in that slot.")
@@ -37,77 +41,87 @@ def cancelSignUp():
             validInput = 1
             participantList[int(slot)-1] = 'None'
             print(f"Success, {name} has been removed from slot #{slot}")
-'''
+
 def viewParticipants():
-    print participantHeader
+    print("View Participants\n=================")
     validInput = 0
 
-    while validInput = 0
-        slot = input slot to view
-        if slot is not numeric
-            print "Error, please enter a number for slot"
-        else
+    while validInput == 0:
+        slot = input(f"Starting slot #[1-{numParticipants}]: ")
+
+        if not slot.isnumeric():
+            print("Error, please enter a number for slot")
+        elif int(slot) > numParticipants or int(slot) < 1:
+            print(f"Invalid input, slot must be a number in range [1-{numParticipants}]")
+        else:
             validInput = 1
     
-    print "Starting Slot: Participant"
+    print("Starting Slot: Participant")
+    slot = int(slot)
 
-    if numParticipants < 11:
-        for i in range(0, slot):
+    if slot < 6 and slot > numParticipants - 6:
+        for i in range(0, numParticipants):
             print(f"{i+1}: {participantList[i]}")
-    if slot < 6
-        for i in range(0, slot)
-            print "{i+1}: {participantList[i]}"
-    else if slot > numParticipants - 5
-        for i in range(slot-6, numParticipants)
-            print "{i+1}: {participantList[i]}"
-    else
-        for i in range(slot-6, slot+4)
-            print "{i+1}: {participantList[i]}"
+    elif slot < 6:
+        for i in range(0, slot+5):
+            print(f"{i+1}: {participantList[i]}")
+    elif slot > numParticipants - 5:
+        for i in range(slot-6, numParticipants):
+            print(f"{i+1}: {participantList[i]}")
+    else:
+        for i in range(slot-6, slot+5):
+            print(f"{i+1}: {participantList[i]}")
 
 
-function saveChanges()
-    print saveChangeHeader
-    answer = ''
+def saveChanges():
+    print("Save Changes\n============")
     validInput = 0
 
-    while validInput = 0
-        answer = input "Save your changes to csv? [y/n]"
+    while validInput == 0:                   #input validation
+        answer = input("Save your changes to CSV? [y/n] ")
 
-        if answer = 'y' or 'n'
+        if answer == 'y':
             validInput = 1
-        else
-            print "Invalid answer, please try again."
-    
-    if answer = 'y'
-        save participantList to csv
-        print "Your changes were saved."
-    else
-        print "Your changes were not saved."
+            #save here
+            file = open('TournamentSlots.csv', 'w')
+            write = csv.writer(file)
+            write.writerow(participantList)
+            print("Your changes were saved.")
+        elif answer == 'n':
+            validInput = 1
+            print("Your changes were not saved.")
+        else:
+            print("Invalid answer, please try again.")
 
-function exit()
-    print exitHeader
-    answer = ''
+def exit():
+    print("Exit\n=====\nAny unsaved changes will be lost.")
     validInput = 0
     global exitVar
 
-    while validInput = 0
-        answer = input "Are you sure you want to exit? [y/n]"
+    while validInput == 0:                  #input validation
+        answer = input("Are you sure you want to exit? [y/n]")
 
-        if answer = 'y' or 'n'
+        if answer == 'y':
             validInput = 1
-        else
-            print "Invalid answer, please try again."
+            exitVar = 1
+        elif answer == 'n':
+            validInput = 1
+        else:
+            print("Invalid answer, please try again.")
 
-    if answer = 'y'
-        exitVar = 1
 
-'''
 
 #start up
 print("Welcome to Tournaments R Us\n============================")
-numParticipants = int(input("Enter the number of participants: "))
+while validInput == 0:                      #input validation
+    numParticipants = input("Enter the number of participants: ")
+    if not numParticipants.isnumeric():
+            print("Error, please enter a number")
+    else:
+        validInput = 1
+        numParticipants = int(numParticipants)
 
-for i in range(0, numParticipants):             #populate participant list
+for i in range(0, numParticipants):         #populate participant list
     participantList.append('None')
 
 print(f"There are {numParticipants} slots available")
@@ -126,7 +140,7 @@ while exitVar == 0:
     """)
     menuInput = input("What do you want to do? Enter [1-5] ")
     
-    if menuInput.isnumeric():
+    if menuInput.isnumeric():               #input validation
         menuInput = int(menuInput)
     else:
         print("Invalid input.")
@@ -135,13 +149,12 @@ while exitVar == 0:
         signUp()
     elif menuInput == 2:
         cancelSignUp()
-    #elif menuInput == 3:
-        #viewParticipants()
-    #elif menuInput == 4:
-        #saveChanges()
+    elif menuInput == 3:
+        viewParticipants()
+    elif menuInput == 4:
+        saveChanges()
     elif menuInput == 5:
-        #exit()
-        exitVar = 1
+        exit()
 
 print("Goodbye!")
     
